c1f0ef31760ed5ef4143b132ff231300cf1c12e9
God'swill Sekav Apya <godswillapya@gmail.com>
2025-06-24 21:30:33 +0100

    feat: implement ingredient submission functionality in MainComponent and enhance button styling

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 
 const MainComponent = () => {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+
+  const ingredientsListsItem = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Form Submitted");
+  };
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form onSubmit={handleSubmit} className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g. beans"
           aria-label="Add ingredient"
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
+
+      <ul>{ingredientsListsItem}</ul>
     </main>
   );
 };
